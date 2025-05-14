65b33c46375cfe6ea3b6178240a355735e044747
Rubeena66 <rubeenashaik766@gmail.com>
2025-05-14 19:55:48 +0530

    Add files via upload

---
+++ IncreasingBy5.java
@@ -0,0 +1,10 @@
+package com.Loops;
+
+public class IncreasingBy5 {
+	public static void main(String[] args) {
+		for(int i=5;i<=50;i=i+5) {
+			System.out.print(i+" ");
+		}
+	}
+
+}
---
+++ OddNumber.java
@@ -0,0 +1,10 @@
+package com.Loops;
+
+public class OddNumber {
+	public static void main(String[] args) {
+		for(int i=1;i<=15;i=i+2) {
+			System.out.print(i+" ");
+		}
+	}
+
+}
